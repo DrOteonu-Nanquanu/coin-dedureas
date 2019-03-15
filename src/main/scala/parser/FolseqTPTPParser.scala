6f70e81645a884ffde1a123fe915e4ad55415450
aeduin <aeduin@nanquanu.org>
2019-03-15 19:41:35 +0100

    string now passes into correct data structure

--- src/main/scala/parser/FolseqTPTPParser.scala
+++ src/main/scala/parser/FolseqTPTPParser.scala
@@ -3,33 +3,33 @@ package parser
 import scala.util.parsing.combinator._
 
 object FolseqTPTPParser extends RegexParsers {
-  def lowercaseID = "[a-z][a-zA-Z]*".r ^^ {TPTPElement.fromAny}
-  def uppercaseID = "[A-Z][a-zA-Z]*".r ^^ {TPTPElement.fromAny}
-  def folPredicate = uppercaseID
-  def variable = lowercaseID
-  def constant = "\"" ~ lowercaseID ~ "\"" ^^ {case _ ~ name ~ _ => TPTPElement.combine(Array("\"", name, "\"")) }
+  def lowercaseID = "[a-z][a-zA-Z]*".r ^^ { new LowercaseID(_) }
+  def uppercaseID = "[A-Z][a-zA-Z]*".r ^^ { new UppercaseID(_) }
+  def folPredicate = uppercaseID ^^ { new FolPredicate(_) }
+  def variable = lowercaseID ^^ { new Variable(_) }
+  def constant = "\"" ~ lowercaseID ~ "\"" ^^ { case _ ~ name ~ _ => new Constant(name) }
 
-  def folFunction = lowercaseID
-  def folTermList : Parser[TPTPElement] = folTerm ~ ", *".r ~ folTermList ^^ {case term ~ _ ~ termList => TPTPElement.combine(Array(term, ", " , termList))} | folTerm
-  def folTerm = folFunction ~ "(" ~ folTermList ~ ")" ^^ {case function ~ _ ~ termList ~ _  => TPTPElement.combine(Array(function, "(", termList, ")"))} | constant | variable
+  def folFunction = lowercaseID ^^ { new FolFunction(_) }
+  def folTermList : Parser[Array[FolTerm]] = folTerm ~ ", *".r ~ folTermList ^^ { case term ~ _ ~ termList => Array(term) ++ termList } | folTerm ^^ { Array(_) }
+  def folTerm = folFunction ~ "(" ~ folTermList ~ ")" ^^ { case function ~ _ ~ termList ~ _  => new FunctionApplication(function, termList) } | constant ^^ { new ConstantTerm(_) } | variable ^^ { new VariableTerm(_) }
 
-  def folAtom : Parser[TPTPElement] = folPredicate ~ "(" ~ folTermList ~ ")" ^^ {case predicate ~ _ ~ termList ~ _ => TPTPElement.combine(Array(predicate, "(", termList, ")"))}
-  def unaryConnective = "not" ^^ {TPTPElement.fromAny}
-  def binaryConnective = "and"  ^^ {TPTPElement.fromAny} | "or" ^^ {TPTPElement.fromAny} | "=>"  ^^ {TPTPElement.fromAny} | "<=>" ^^ {TPTPElement.fromAny}
+  def folAtom : Parser[AtomStatement] = folPredicate ~ "(" ~ folTermList ~ ")" ^^ { case predicate ~ _ ~ termList ~ _ => new AtomStatement(predicate, termList) }
+  def unaryConnective = "not" ^^ { (x) => new Not() }
+  def binaryConnective : Parser[BinaryConnective] = "and"  ^^ { (x) => new And() } | "or" ^^ { (x) => new Or() } | "=>"  ^^ { (x) => new IfThen()} | "<=>" ^^ { (x) => new Iff() }
 
-  def statement : Parser[TPTPElement] = unaryConnective ~ statement ^^ {case connective ~ stmt => TPTPElement.combine(Array(connective, stmt))} |
-    "(" ~ statement ~ binaryConnective ~ statement ~ ")" ^^ {case _ ~ statement1 ~ connective ~ statement2 ~ _ => TPTPElement.combine(Array(statement1, connective, statement2))} |
+  def statement : Parser[Statement] = unaryConnective ~ statement ^^ { case connective ~ stmt => new UnaryConnectiveStatement(connective, stmt) } |
+    "(" ~ statement ~ binaryConnective ~ statement ~ ")" ^^ { case _ ~ statement1 ~ connective ~ statement2 ~ _ => new BinaryConnectiveStatement(statement1, connective, statement2) } |
     quantifiedFormula | folAtom
   //currently, each statement with a binaryConnective has to be surrounded by brackets, this shouldn't be needed and should change in the future
-  def quantifiedFormula = quantifier ~ "[" ~ quantifierArguments ~ "]:" ~ statement ^^ {case quant ~ _ ~ quantArguments ~ _ ~ stmt => TPTPElement.combine(Array(quant, "[", quantArguments, "]:", stmt))}
-  def quantifier = "!" ^^ {TPTPElement.fromAny} | "?" ^^ {TPTPElement.fromAny}
-  def quantifierArguments = variable ~ " from " ~ constantSet ^^ {case v ~ _ ~ constant_set => TPTPElement.combine(Array(v, constant_set))} | variableList ^^ {TPTPElement.fromAny}
-  def variableList: Parser[TPTPElement] = variable | variable ~ variableList ^^ {case v ~ v_list => TPTPElement.combine(Array(v, v_list))}
-  def constantSet = "{" ~ constantSetElements ~ "}" ^^ {case _ ~ elements ~ _ => TPTPElement.combine(Array("{", elements, "}"))} | patternVar
-  def patternVar =  lowercaseID ~ "_" ^^ {case id ~ _ => TPTPElement.combine(Array(id, "_"))}
-  def constantSetElements: Parser[TPTPElement] = constant | constant ~ constantSetElements ^^ {case const ~ constSetElements => TPTPElement.combine(Array(const, constSetElements))}
+  def quantifiedFormula = quantifier ~ "[" ~ quantifierArguments ~ "]:" ~ statement ^^ { case quant ~ _ ~ quantArguments ~ _ ~ stmt => new QuantifiedStatement(quant, quantArguments, stmt) }
+  def quantifier = "!" ^^ {(x) => new ForAll()} | "?" ^^ {(x) => new Exists()}
+  def quantifierArguments = variable ~ " from " ~ constantSet ^^ {case v ~ _ ~ constant_set => new ConstantSetQuantifierArguments(v, constant_set)} | variableList ^^ { new BasicQuantifierArguments(_) }
+  def variableList: Parser[Array[Variable]] = variable ^^ { Array(_) } | variable ~ variableList ^^ { case v ~ v_list => Array(v) ++ v_list }
+  def constantSet = "{" ~ constantSetElements ~ "}" ^^ {case _ ~ elements ~ _ => new BasicConstantSet(elements)} | patternVar
+  def patternVar =  lowercaseID ~ "_" ^^ { case id ~ _ => new PatternVar(id) }
+  def constantSetElements: Parser[Array[Constant]] = constant ^^ { Array(_) } | constant ~ constantSetElements ^^ { case const ~ constSetElements => Array(const) ++ constSetElements }
 
-  def fofsequa_document: Parser[String] = statement ^^ {case stmt => stmt.toFOF} | statement ~ fofsequa_document ^^ {case stmt ~ doc => stmt.toFOF + doc}
+  def fofsequa_document: Parser[Array[Statement]] = statement ^^ { Array(_) } | statement ~ fofsequa_document ^^ {case stmt ~ doc => Array(stmt) ++ doc }
 }
 
 case class TPTPElement(content: String, isConjecture: Boolean = false) {
@@ -56,14 +56,12 @@ object TPTPElement {
 
   def fromAny(element: Any): TPTPElement = new TPTPElement(element.toString, false)
 }
-
-case class FofsequaDocument(statements: Array[Statement])
-
+    
 sealed abstract class Statement
 case class BinaryConnectiveStatement(pre_statement: Statement, connective: BinaryConnective, post_statement: Statement) extends Statement
 case class UnaryConnectiveStatement(connective: UnaryConnective, post_statement: Statement) extends Statement
 case class QuantifiedStatement(quantifier: Quantifier, arguments: QuantifierArguments, statement: Statement) extends Statement
-case class AtomStatement(predicate: FolPredicate, terms: Array[FolTerm])
+case class AtomStatement(predicate: FolPredicate, terms: Array[FolTerm]) extends Statement
 
 sealed abstract class Quantifier
 case class ForAll() extends Quantifier
@@ -86,10 +84,10 @@ sealed abstract class UnaryConnective
 case class Not() extends UnaryConnective
 
 sealed abstract class BinaryConnective
-case class And()
-case class Or()
-case class IfThen()
-case class Iff()
+case class And() extends BinaryConnective
+case class Or() extends BinaryConnective
+case class IfThen() extends BinaryConnective
+case class Iff() extends BinaryConnective
 
 case class FolPredicate(name: UppercaseID)
 
